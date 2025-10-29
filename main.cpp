b35297f6e83d1eda1923dd6b9488006f1c9eca24
Josh Lowden <lowdenj1@msu.edu>
2025-10-29 14:12:07 -0400

    Refactor game selection and retry logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "typing_game.hpp"
 #include "wordle.hpp"
 #include <iostream>
+#include <cctype>
+#include <limits>
 
 std::string Print_Menu()
 {
@@ -10,10 +12,62 @@ std::string Print_Menu()
     std::cout << std::string(50, '-') << '\n';
 
     std::string choice {};
-    std::cout << "\t     Select Game: "; 
+    std::cout << "\t       Select Game: "; 
     std::cin >> choice;
     
     return choice;
+}
+
+void typing_game()
+{
+    char retry {};
+    while (std::toupper(retry) != 'N')
+    {
+        TypingGame game;
+        if (game.play() == 0)
+        {
+            std::cout << "Would you like to rety? (Y/N): ";
+            std::cin >> retry;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            retry = std::toupper(retry);
+
+            while (retry != 'Y' && retry != 'N')
+            {
+                std::cout << "Invalid input, try again (Y/N): ";
+                std::cin >> retry;
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                retry = std::toupper(retry);
+            }
+        }
+        if (retry == 'N')
+            break;
+    }
+}
+
+void wordle()
+{
+    char retry {};
+    while (std::toupper(retry) != 'N')
+    {
+        Wordle game;
+        if (game.play() == 0)
+        {
+            std::cout << "Would you like to rety? (Y/N): ";
+            std::cin >> retry;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            retry = std::toupper(retry);
+
+            while (retry != 'Y' && retry != 'N')
+            {
+                std::cout << "Invalid input, try again (Y/N): ";
+                std::cin >> retry;
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                retry = std::toupper(retry);
+            }
+        }
+        if (retry == 'N')
+            break;
+    }
 }
 
 int main()
@@ -26,16 +80,10 @@ int main()
     }
 
     std::cout << "\n\n\n\n\n\n";
+
     if (game_selected == "1")
-    {
-        std::cout << "Selected Typing Game";
-        TypingGame game;
-        game.play();
-    }
+        typing_game();
+
     else
-    {
-        Wordle game;
-        if (game.play() == 0)
-            return 0;
-    }
+        wordle();
 }
